06e040c2aab186377240a40d72f9c96c16e0cf98
Lavender Glaab <piggehperson@gmail.com>
2021-04-14 19:46:28 -0400

    File structure, start on theme

---
+++ lib/ui/common/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData kyouTheme = ThemeData(
+    // This is the theme of your application.
+    //
+    // Try running your application with "flutter run". You'll see the
+    // application has a blue toolbar. Then, without quitting the app, try
+    // changing the primarySwatch below to Colors.green and then invoke
+    // "hot reload" (press "r" in the console where you ran "flutter run",
+    // or simply save your changes to "hot reload" in a Flutter IDE).
+    // Notice that the counter didn't reset back to zero; the application
+    // is not restarted.
+    brightness: Brightness.light,
+    primarySwatch: Colors.deepPurple,
+    scaffoldBackgroundColor: Color.fromRGBO(224, 224, 240, 1.0));
